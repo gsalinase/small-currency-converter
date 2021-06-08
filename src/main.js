4bbe75e3aefc8841ca960d9d4376ae0519fe76a2
Gonzalo Salinas <gsalinase91@gmail.com>
2021-06-08 08:33:35 -0400

    styles add to global app config

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+// Importing the global css file
+import '@/css/styles.css';
+
+const app = createApp(App)
+app.use(store);
+app.use(router);
+app.mount('#app');
